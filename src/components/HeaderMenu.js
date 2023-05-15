5286115491071f52d1f3fb2e05ec9c85d3888234
ThisisDC <danielecaldarigi05@gmail.com>
2023-05-15 21:11:06 +0200

    changed some particulars

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -2,30 +2,29 @@ import classes from "./HeaderMenu.module.css";
 import { NavLink } from "react-router-dom";
 
 function HeaderMenu(props) {
-  function onNavLinkClick() {
+  function onNavCloseRequestClick() {
     props.onLinkClick();
   }
 
   return (
-    <>
+    <div className={classes.backdrop} onClick={onNavCloseRequestClick}>
       <div className={classes.menu}>
-        <div className={classes.x} onClick={onNavLinkClick}>
-          <img src={require("../images/298889_x_icon.png")} alt="close" />
+        <div className={classes.x} onClick={onNavCloseRequestClick}>
+          &#x2715;
         </div>
-
         <ul className={classes.list}>
-          <NavLink to="/" onClick={onNavLinkClick}>
+          <NavLink to="/" onClick={onNavCloseRequestClick}>
             <li>HOME</li>
           </NavLink>
-          <NavLink to="chi-siamo" onClick={onNavLinkClick}>
+          <NavLink to="chi-siamo" onClick={onNavCloseRequestClick}>
             <li>CHI SIAMO</li>
           </NavLink>
-          <NavLink to="progetti" onClick={onNavLinkClick}>
+          <NavLink to="progetti" onClick={onNavCloseRequestClick}>
             <li>PROGETTI</li>
           </NavLink>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 export default HeaderMenu;
